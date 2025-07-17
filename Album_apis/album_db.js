c4c12ea56493317dc37b0b3f3bf074d79b6c6c22
Sam Glynn <samglynn@Sams-MacBook-Pro.local>
2025-07-17 09:27:19 -0500

    Updated fetchData to take mulitple endpoint call

--- Album_apis/album_db.js
+++ Album_apis/album_db.js
@@ -1,75 +1,40 @@
 //This is an example of a GET call to Rapid API to search for a certain Song
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     'x-rapidapi-host': 'shazam.p.rapidapi.com',
-//     'X-RapidAPI-Key': '2900b89c69mshc8728a11ce7dfe0p1261e1jsneac4a5f79fc6'
-//   }
-// };
-
-// fetch('https://shazam.p.rapidapi.com/search?term=', options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
+//fetchData();
 
 async function fetchData() {
   try {
-    const collection1 = document.getElementById("collection1").value.toLowerCase();
+    //const collection1 = 'paint%20it%20black';
+    let collection1 = document.getElementById('collection1').value.toLowerCase();
+    let collection2 = document.getElementById('collection2').value.toLowerCase();
+    let collection3 = document.getElementById('collection3').value;
 
-      const options = {
+    const options = {
       method: "GET",
       headers: {
         "x-rapidapi-host": "shazam-api6.p.rapidapi.com",
-        "x-rapidapi-key": "2900b89c69mshc8728a11ce7dfe0p1261e1jsneac4a5f79fc6",
+        "x-rapidapi-key": "968c1290efmsh9dbe73847996103p17e33bjsnfc07ebe98661",
       },
     };
 
-    const response = await fetch(
-      `https://shazam-api6.p.rapidapi.com/shazam/search_track/${collection1}`,
+     fetch(
+      'https://shazam-api6.p.rapidapi.com/shazam/search_track/?query=' + collection1,
       options
-    );
+    ).then(response => response.json())   
+    .then(response => console.log(response))
 
-    if (!response.adamid) {
-      throw new Error("Cannot fetch resources");
-    }
+    fetch(
+      'https://shazam-api6.p.rapidapi.com/shazam/search_artist/?query=' + collection2,
+      options
+    ).then(response2 => response2.json())   
+    .then(response2 => console.log(response2))
 
-    const data = await response.json();
-    console.log(data);
+    fetch(
+      'https://shazam-api6.p.rapidapi.com/shazam/about_artist?artist_id=' + collection3,
+      options
+    ).then(response3 => response3.json())   
+    .then(response3 => console.log(response3))
+
   } catch (error) {
     console.log(error);
   }
 }
-
-// This is an example of a GET Artist call to Rapid API to search
-// for a certain artist with their Artist_id (adamid)
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     'x-rapidapi-host': 'shazam-api6.p.rapidapi.com',
-//     'x-rapidapi-key': '2900b89c69mshc8728a11ce7dfe0p1261e1jsneac4a5f79fc6'
-//   }
-// };
-
-// fetch('https://shazam-api6.p.rapidapi.com/shazam/search_artist/?query=', options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
-
-// This is an example of a GET Artist details call to Rapid API to search for the
-// list albums from the given Artist_id(adamid) value from Artist GET Call from there you will get a list of albums
-// const url = 'https://shazam-api6.p.rapidapi.com/shazam/about_artist';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'x-rapidapi-key': '2900b89c69mshc8728a11ce7dfe0p1261e1jsneac4a5f79fc6',
-// 		'x-rapidapi-host': 'shazam-api6.p.rapidapi.com'
-// 	}
-// };
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
